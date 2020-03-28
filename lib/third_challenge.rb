dfd4f2e5c3122d97bf567c6d842d1336daea00ae
Curtis Chadwell <laurencechadwell@gmail.com>
2020-03-28 00:28:55 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,29 +2,44 @@ def third_challenge
   epic_tragedy = {
    #first_level
    :montague => {
+      #second_level
       :patriarch => {
-        #your key/value pairs here
+        #third_level
+        :name => "Lord Montague",
+        :age => "53"
         },
+        #second_level
       :matriarch => {
-        #your key/value pairs here
+        #third_level
+        :name => "Lady Montague",
+        :age => "54"
         },
+        #second_level
       :hero => {
-        #your key/value pairs here
+        #third_level
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
+        
         },
+        #second_level
       :hero_friends => []
    }, 
    #first_level
    :capulet => {
-     
+      #second_level
       :patriarch => {
         #your key/value pairs here
         },
+        #second_level
       :matriarch => {
         #your key/value pairs here
         },
+        #second_level
       :heroine => {
         #your key/value pairs here
         },
+        #second_level
       :heroine_friends => []
    }
   }

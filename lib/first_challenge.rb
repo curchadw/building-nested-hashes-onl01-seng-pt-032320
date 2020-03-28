431587f2897322a452474170050487dee74f336b
Curtis Chadwell <laurencechadwell@gmail.com>
2020-03-28 00:17:05 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,14 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     #first_level
+     :montague =>{
+       #second level
+       :patriarch =>{},
+       :matriarch =>{},
+       :hero => {},
+       :hero_friends =>{}
+     },
+     #first_level
+     :capulet => {}
+   }
 end
